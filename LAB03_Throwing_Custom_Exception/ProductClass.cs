6339ad4fa6ff99369a9ab71d4aeb8b557b17f1e4
19-anel <lyraresflovilla@gmail.ccom>
2025-09-15 20:39:10 +0800

    Add project files.

---
+++ LAB03_Throwing_Custom_Exception/ProductClass.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB03_Throwing_Custom_Exception
+{
+    internal class ProductClass
+    {
+        private int _Quantity;
+        private double _SellingPrice;
+        private string _ProductName, _Category, _ManufacturingDate, _ExpirationDate, _Description;
+
+        public ProductClass(string ProductName, string Category, string MfgDate, string ExpDate, 
+            double Price, int Quantity, string Description) 
+        {
+            this._Quantity = Quantity;
+            this._SellingPrice = Price;
+            this._ProductName = ProductName;
+            this._Category = Category;
+            this._ManufacturingDate = MfgDate;
+            this._ExpirationDate = ExpDate;
+            this._Description = Description;
+        }
+
+        public string ProductName 
+        {
+            get { return this._ProductName; }
+            set { this._ProductName = value; }
+        }
+
+        public string Category
+        {
+            get { return this._Category; }
+            set { this._Category = value; }
+        }
+
+        public string ManufacturingDate
+        {
+            get { return this._ManufacturingDate; }
+            set { this._ManufacturingDate = value; }
+        }
+
+        public
+    }
+}
---
+++ LAB03_Throwing_Custom_Exception/frmAddProduct.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LAB03_Throwing_Custom_Exception
+{
+    public partial class frmAddProduct : Form
+    {
+        public frmAddProduct()
+        {
+            InitializeComponent();
+        }
+
+        private void frmAddProduct_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAddProduct_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
